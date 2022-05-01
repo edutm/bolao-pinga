f85dc070dbc0e404b93797e764ab51904d93bf43
Eduardo Teixeira Monteiro <edutm87@hotmail.com>
2022-05-01 16:45:29 -0300

    primeiro commit

---
+++ bolao-pinga/src/main/java/br/com/bolaopinga/bolaopinga/BolaoPingaApplication.java
@@ -0,0 +1,13 @@
+package br.com.bolaopinga.bolaopinga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolaoPingaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolaoPingaApplication.class, args);
+	}
+
+}
