74a1032ea0c70924ed4ea5d3842b1eb17807441e
Eduardo Teixeira Monteiro <edutm87@hotmail.com>
2022-07-08 18:29:20 -0300

    ajsute cors front

--- front/src/services/service.js
+++ front/src/services/service.js
@@ -8,6 +8,7 @@ const api = axios.create({
   baseURL: 'https://bolao-pinga-dev.herokuapp.com/api/',
   headers: {
     'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
     //Authorization : loginDto ? `Bearer ${loginDto.token}` : ''
   }
 });
